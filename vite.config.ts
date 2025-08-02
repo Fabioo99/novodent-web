67fc30d0fcf4f5da6e3c7c8665455224559b7f28
Fabio Ochoa <fabioochoa26@gmail.com>
2025-08-02 15:15:02 -0400

    Fix Vite base path and vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // Importante para deploy en dominio raíz
   server: {
     host: "::",
     port: 8080,
